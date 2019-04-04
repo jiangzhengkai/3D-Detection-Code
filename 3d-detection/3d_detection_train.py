c87ca4a926f2c449669073541f92a08e47498709
jiangzhengkai <kaikaijiang.jzk@gmail.com>
2019-04-04 11:56:11 +0800

    update

---
+++ 3d-detection/3d_detection_train.py
@@ -0,0 +1,47 @@
+#### python package
+import 
+
+
+
+
+
+
+
+
+### main function
+def train(config, model_dir, result_path=None, resume=False):
+    ####### load config #######
+
+
+
+    ####### build network #######
+
+
+ 
+    ####### dataloader #######
+
+
+
+
+    ####### optimizer #######
+
+
+
+    ####### training #######
+
+
+def evaluate(config, model_dir, result_path=None, model_path=None, measure_time=False, batch_size=None):
+
+    ####### load config #######
+
+
+
+    ####### build network #######
+
+
+
+    ####### dataloader #######
+
+
+
+    ####### evaluation #######
