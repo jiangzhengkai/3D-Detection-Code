d3f9414148c8bbc40d59ceef656f3968926f9974
Jiang ZhengKai <kaikaijiang.jzk@gmail.com>
2019-04-06 21:58:03 +0800

    Update 3d_detection_train.py

--- 3d-detection/3d_detection_train.py
+++ 3d-detection/3d_detection_train.py
@@ -1,5 +1,8 @@
 #### python package
-import 
+import pathlib
+import torch
+from 3d-detection.config import cfg
+
 
 
 
@@ -12,22 +15,30 @@ import
 def train(config, model_dir, result_path=None, resume=False):
     ####### load config #######
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
     ####### build network #######
+    net = build_network(config).to(device)
+    
+    
 
 
  
     ####### dataloader #######
-
+    dataloader = build_dataloader(config, training=True, voxel_generator, target_assigner)
 
 
 
     ####### optimizer #######
+    optimizer = build_optimizer(config)
+    lr_scheduler = build_scheduler(config)
 
 
 
     ####### training #######
+    data_iter = iter(dataloader)
+    
 
 
 def evaluate(config, model_dir, result_path=None, model_path=None, measure_time=False, batch_size=None):
