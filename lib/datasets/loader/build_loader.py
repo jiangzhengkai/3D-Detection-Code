1c7fcc19ff7426f0c8afeb4dc0d92aefc47f211a
jiangzhengkai <jiangzhengkai@megvii.com>
2019-05-08 13:48:21 +0800

    update

--- lib/datasets/loader/build_loader.py
+++ lib/datasets/loader/build_loader.py
@@ -1,35 +1,34 @@
-from torch.utils.data import Dataset
-
-
-voxel_generator
-target_assigners
-
-
-class DatasetWarpper(Dataset):
-    def __init__(self, dataset):
-        self._dataset = dataset
-    def __len__(self):
-        return len(self._dataset)
-    def __getitem__(self, idx):
-        return self._dataset[idx]
-    @property
-    def dataset(self):
-        return self._dataset
+from functools import partial
+from lib.core.voxel.voxel_generator import VoxelGenerator
+from lib.core.target.target_assigner import TargetAssiginer
 
 
 def build_dataset(config, training):
 
-    voxel_generator = build_voxel_generator(config)
+    voxel_generator = VoxelGenerator(voxel_size=config.voxel.voxel_size,
+				     point_cloud_range=config.voxel.point_cloud_range,
+				     max_num_points=config.voxel.max_num_points,
+				     max_voxels=config.voxel.max_voxels)
+
+
+
+    target_assiginer = TargetAssiginer(box_coder=,
+				       anchor_generators=,
+				       region_similarity_calculator=,
+				       positive_fraction=,
+				       sample_size=512)
+
     grid_size = voxel_generator.grid_size
     feature_map_size = grid_size[:2] // 2
     
+    prep_func = 
 
 
+    dataset = KittiDataset()
 
 
 def build_dataloader(config, training):
     dataset = build_dataset(config, training)
-    dataset = DatasetWarpper(dataset)
     
     batch_size = config.train.batch_size if training else config.eval.batch_size
     num_workers = config.train.num_workers if training else config.eval.num_workers
