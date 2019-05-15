837aa40d65c857f2659f429feef6af0510231656
jiangzhengkai <jiangzhengkai@megvii.com>
2019-05-15 18:48:13 +0800

    update

--- lib/models/voxelnet.py
+++ lib/models/voxelnet.py
@@ -143,5 +143,79 @@ class VoxelNet(nn.Module):
         return predict_dicts
 
 
-    def loss(self, example, pred_dict):
+    def loss(self, example, preds_dicts):
+        voxels = example["voxels"]
+        num_points = example["num_points"]
+        coordinates = example["coordinates"]
+        batch_anchors = example["anchors"]
+        batch_size_dev = batch_anchors[0].shape[0]
+
+   
+
+        rets = []
+        for task_id, pred_dict in enumerate(preds_dicts): 
+            box_preds = pred_dict["box_preds"] 
+            cls_preds = pred_dict["cls_preds"]
+     
+            labels = example["labels"][task_id]
+            reg_targets = example["reg_targets"][task_id]
+            
+            cls_weights, reg_weights, cared = prepare_loss_weights(labels,
+                                                                   pos_cls_weight=self._pos_cls_weight,
+                                                                   neg_cls_weight=self._neg_cls_weight,
+                                                                   loss_norm_type=self._noss_norm_type,
+                                                                   dtype=voxels.dtype)
+
+            loc_loss, cls_loss = create_loss(self._loc_loss_factor,
+                                             self._cls_loss_factor,
+                                             box_preds=box_preds,
+                                             reg_targets=reg_targets,
+                                             reg_weights=reg_weights,
+                                             cls_preds=cls_preds,
+                                             cls_targets=cls_targets,
+                                             cls_weights=cls_weights,
+                                             num_class=self._num_classes[task_id],
+                                             encode_rad_error_by_sin=self._encode_rad_error_by_sin,
+                                             encode_background_as_zeros=self._encode_background_as_zeros,
+                                             box_code_size=self._box_coders[task_id].code_size)
+             
+            loc_loss_reduced = loc_loss.sum() / batch_size_dev
+            loc_loss_reduced *= self._loc_loss_weight
     
+            cls_pos_loss, cls_neg_loss = get_pos_neg_loss(cls_loss, labels)
+            cls_pos_loss /= self._pos_cls_weight
+            cls_neg_loss /= self._neg_cls_weight
+           
+            cls_loss_reduced = cls_loss.sum() / batch_size_dev
+            cls_loss_reduced *= self._cls_loss_weight
+
+            loss = loc_loss_reduced + cls_loss_reduced  
+       
+            if self._use_direction_classifier:
+                dir_targets = get_direction_target(example["anchors"][task_id],
+                                                   reg_targets,
+                                                   dir_offset=self._direction_offset)
+                dir_logits = pred_dict["dir_cls_preds"].view(batch_size_dev, -1, 2)
+                weights = (labels >0).type_as(dir_logits)
+                weights /= torch.clamp(weights.sum(-1, keep_dim=True))
+                dir_loss = self._dir_loss_factor(dir_logits,
+                                                 dir_targets,
+                                                 weights=weights)
+
+                loss += dir_loss * self._direction_loss_weight
+            ret = {
+                "loss": loss,
+                "cls_loss": cls_loss,
+                "loc_loss": loc_loss,
+                "cls_pos_loss": cls_pos_loss,
+                "cls_neg_loss": cls_neg_loss,
+                "cls_preds": cls_preds,
+                "dir_loss_reduced": dir_loss if self._use_dirction_classifier else None,
+                "cls_loss_reduced": cls_loss_reduced,
+                "loc_loss_reduced": loc_loss_reduced,
+                "cared": cared,
+                  }
+            rets.append(ret)
+
+
+
