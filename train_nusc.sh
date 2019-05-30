ff26ce2ee77f11a9a80a194466d99b9ab303263f
jiangzhengkai <jiangzhengkai@megvii.com>
2019-05-30 14:23:11 +0800

    update

--- train_nusc.sh
+++ train_nusc.sh
@@ -1,2 +1,2 @@
 #~/bin/bash
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --master_addr="127.0.0.1" --master_port=8666 ./tools/train.py --cfg configs/all_nuscene_fhd.yaml --model_dir nusc_output
+rlaunch --cpu 20 --gpu 4 --memory 150000 --  python -m torch.distributed.launch --nproc_per_node=4 --master_addr="127.0.0.1" --master_port=8666 ./tools/train.py --cfg configs/all_nuscene_fhd.yaml --model_dir nusc_output
