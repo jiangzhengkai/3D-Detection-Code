d7beb339b18f5f7980023db044f75cfe6700b6b7
jiangzhengkai <jiangzhengkai@megvii.com>
2019-06-02 11:01:00 +0800

    update

--- train_kitti.sh
+++ train_kitti.sh
@@ -1,2 +1,2 @@
 #~/bin/bash
-python -m torch.distributed.launch --nproc_per_node=6 --master_addr="127.0.0.1" --master_port=8888 ./tools/train.py --cfg configs/car_fhd_kitti.yaml --model_dir kitti
+python -m torch.distributed.launch --nproc_per_node=6 --master_addr="127.0.0.1" --master_port=8882 ./tools/train.py --cfg configs/car_fhd_kitti.yaml --model_dir kitti
